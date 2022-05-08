8080eca00c6f6dad9d0e57d6548603ff83d36cdb
Gareth Francis <gfrancis.dev@gmail.com>
2022-05-08 17:31:11 +0100

    Beef up the local gui

--- backend/server.py
+++ backend/server.py
@@ -3,10 +3,16 @@ from flask import Flask, request
 from flask_api import status
 import pyautogui
 import qrcode
+import sys
+import os
+import io
+import signal
 import time
 import socket
+import tempfile
 from PIL import Image
 import PySimpleGUI as psg
+from threading import Thread, Event
 
 app = Flask(
     "numpad-backend",
@@ -53,8 +59,40 @@ def get_server_ip():
         s.close()
     return ip
 
-def show_pairing_gui(qr_img):
-    qr_img.show(title="Scan to connect")
+def show_pairing_gui(qr_img, qr_data, sentinel):
+
+    temp_img = tempfile.NamedTemporaryFile(delete=False)
+    b = io.BytesIO()
+    qr_img.save(b, format='PNG')
+    temp_img.write(b.getvalue())
+    img_path = temp_img.name
+    temp_img.close()
+
+    layout = [
+        [psg.Text("Scan code to connect")],
+        [psg.Text("Close window to shut down server")],
+        [psg.Image(img_path)],
+        [psg.Text(f"Server URL: {qr_data}")],
+    ]
+    win = psg.Window(
+        title="Offscreen Keyboard", 
+        layout=layout
+    )
+    
+    while not sentinel.is_set():
+        event, values = win.read()
+        if event ==  psg.WIN_CLOSED:
+            break
+    win.close()
+
+    try:
+        os.remove(img_path)
+    except Exception:
+        pass
+
+    # TODO: This could be cleaner, but works
+    sig = getattr(signal, "SIGKILL", signal.SIGTERM)
+    os.kill(os.getpid(), sig)
 
 if __name__ == '__main__':
     # Locally, use qr to point mobile to server
@@ -62,7 +100,11 @@ if __name__ == '__main__':
     server_port = 8080
     qr_data = f"http://{get_server_ip()}:{server_port}"
     qr_img = qrcode.make(qr_data)
-    show_pairing_gui(qr_img)
+
+    sentinel = Event()
+    gui_thread = Thread(target=show_pairing_gui, args=(qr_img, qr_data, sentinel))
+    gui_thread.start()
 
     # TODO: https support, or something like that..Do our best to not internet-host the keyboard :)
     app.run(host='0.0.0.0', port=server_port)
+    sentinel.set()
