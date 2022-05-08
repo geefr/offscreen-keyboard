45592faba9082116c813c0af385c76822865aaec
Gareth Francis <gfrancis.dev@gmail.com>
2022-05-08 17:07:50 +0100

    Generate qr code for pairing

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,11 @@ from urllib import request
 from flask import Flask, request
 from flask_api import status
 import pyautogui
+import qrcode
 import time
+import socket
+from PIL import Image
+import PySimpleGUI as psg
 
 app = Flask(
     "numpad-backend",
@@ -36,9 +40,29 @@ def keypress():
     pyautogui.keyUp(key)
     return "", status.HTTP_200_OK
 
+def get_server_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        return None
+    finally:
+        s.close()
+    return ip
+
+def show_pairing_gui(qr_img):
+    qr_img.show(title="Scan to connect")
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-    # TODO
-    # Generate auth guid
-    # Make qrcode - server url and auth guid
-    # Display desktop window for device pairing
+    # Locally, use qr to point mobile to server
+    # TODO: And pass over an auth token for the session!
+    server_port = 8080
+    qr_data = f"http://{get_server_ip()}:{server_port}"
+    qr_img = qrcode.make(qr_data)
+    show_pairing_gui(qr_img)
+
+    # TODO: https support, or something like that..Do our best to not internet-host the keyboard :)
+    app.run(host='0.0.0.0', port=server_port)
